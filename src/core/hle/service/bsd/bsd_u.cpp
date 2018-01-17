8bb88a135bcec19e9d89ef1d2fcdd843613cf0f5
Frederic Meyer <binarynoob96@gmail.com>
2018-01-17 22:03:20 +0100

    bsd: attempt to get past socket()

--- src/core/hle/service/bsd/bsd_u.cpp
+++ src/core/hle/service/bsd/bsd_u.cpp
@@ -19,9 +19,27 @@ void BSD_U::RegisterClient(Kernel::HLERequestContext& ctx) {
     rb.Push<u32>(0);
 }
 
+void BSD_U::Socket(Kernel::HLERequestContext& ctx) {
+    IPC::RequestParser rp{ctx};
+
+    u32 domain = rp.Pop<u32>();
+    u32 type = rp.Pop<u32>();
+    u32 protocol = rp.Pop<u32>();
+
+    LOG_WARNING(Service, "(STUBBED) called domain=%d type=%d protocol=%d", domain, type, protocol);
+
+    u32 fd = next_fd++;
+
+    IPC::RequestBuilder rb{ctx, 3};
+
+    rb.Push<u32>(fd);
+    rb.Push<u32>(0); // bsd errno (no error)
+}
+
 BSD_U::BSD_U() : ServiceFramework("bsd:u") {
     static const FunctionInfo functions[] = {
-        {0, &BSD_U::RegisterClient, "RegisterClient"}
+        {0, &BSD_U::RegisterClient, "RegisterClient"},
+        {2, &BSD_U::Socket, "Socket"}
     };
     RegisterHandlers(functions);
 }
