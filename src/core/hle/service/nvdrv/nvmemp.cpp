986be67261b3179ab9f505b35bae77aaa10aeed8
David Marcec <dmarcecguzman@gmail.com>
2018-01-20 19:34:18 -0800

    clang-format6.0

--- src/core/hle/service/nvdrv/nvmemp.cpp
+++ src/core/hle/service/nvdrv/nvmemp.cpp
@@ -13,7 +13,8 @@ namespace Nvidia {
 
 NVMEMP::NVMEMP() : ServiceFramework("nvmemp") {
     static const FunctionInfo functions[] = {
-        {0, &NVMEMP::Unknown0, "Unknown0"}, {1, &NVMEMP::Unknown1, "Unknown1"},
+        {0, &NVMEMP::Unknown0, "Unknown0"},
+        {1, &NVMEMP::Unknown1, "Unknown1"},
     };
     RegisterHandlers(functions);
 }
