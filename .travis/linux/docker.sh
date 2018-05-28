cd378a7bff97710a09e3611dfe18775aa388144d
NekokoTime <38867013+NekokoTime@users.noreply.github.com>
2018-05-28 09:06:11 -0300

    Pull request #479 - Use Ninja for Travis builds
    Add Update fron Cmake 3.11.2 for linux

--- .travis/linux/docker.sh
+++ .travis/linux/docker.sh
@@ -4,8 +4,8 @@ apt-get update
 apt-get install -y build-essential git libqt5opengl5-dev libsdl2-dev libssl-dev python qtbase5-dev wget ninja-build
 
 # Get a recent version of CMake
-wget https://cmake.org/files/v3.10/cmake-3.10.1-Linux-x86_64.sh
-sh cmake-3.10.1-Linux-x86_64.sh --exclude-subdir --prefix=/ --skip-license
+wget https://cmake.org/files/v3.11/cmake-3.11.2-Linux-x86_64.sh
+sh cmake-3.11.2-Linux-x86_64.sh --exclude-subdir --prefix=/ --skip-license
 
 cd /yuzu
 
