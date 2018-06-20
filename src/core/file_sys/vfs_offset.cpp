1cc64195249fba75f9d15565c34ff59d528fb04b
Zach Hilman <zachhilman@gmail.com>
2018-06-19 21:52:41 -0400

    Finished OffsetVfsFile

--- src/core/file_sys/vfs_offset.cpp
+++ src/core/file_sys/vfs_offset.cpp
@@ -2,4 +2,68 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-namespace FileSys {} // namespace FileSys
+#include "core/file_sys/vfs_offset.h"
+
+namespace FileSys {
+
+OffsetVfsFile::OffsetVfsFile(VfsFile&& file, u64 offset, u64 size)
+    : file(std::make_unique<VfsFile>(file)), offset(offset), size(size) {}
+
+bool OffsetVfsFile::IsReady() {
+    return file->IsReady();
+}
+
+bool OffsetVfsFile::IsGood() {
+    return file->IsGood();
+}
+
+void OffsetVfsFile::ResetState() {
+    file->ResetState();
+}
+
+std::string OffsetVfsFile::GetName() {
+    return file->GetName();
+}
+
+u64 OffsetVfsFile::GetSize() {
+    return size;
+}
+
+bool OffsetVfsFile::Resize(u64 new_size) {
+    if (offset + new_size < file->GetSize()) {
+        size = new_size;
+        return true;
+    }
+
+    return false;
+}
+
+std::shared_ptr<VfsDirectory> OffsetVfsFile::GetContainingDirectory() {
+    return file->GetContainingDirectory();
+}
+
+bool OffsetVfsFile::IsWritable() {
+    return file->IsWritable();
+}
+
+bool OffsetVfsFile::IsReadable() {
+    return file->IsReadable();
+}
+
+std::vector<u8> OffsetVfsFile::ReadBytes(u64 r_offset, u64 r_length) {
+    return file->ReadBytes(offset + r_offset, std::min(r_offset + r_length, size));
+}
+
+u64 OffsetVfsFile::WriteBytes(const std::vector<u8>& data, u64 r_offset) {
+    auto end = data.end();
+    if (data.size() + r_offset > size)
+        end = data.begin + size - r_offset;
+
+    return file->WriteBytes(std::vector<u8>(data.begin(), end), r_offset + offset);
+}
+
+bool OffsetVfsFile::Rename(const std::string& name) {
+    return file->Rename(name);
+}
+
+} // namespace FileSys
--- src/core/file_sys/vfs_offset.h
+++ src/core/file_sys/vfs_offset.h
@@ -3,5 +3,30 @@
 // Refer to the license.txt file included.
 
 #pragma once
+#include "vfs.h"
 
-namespace FileSys {} // namespace FileSys
+namespace FileSys {
+
+struct OffsetVfsFile : public VfsFile {
+    OffsetVfsFile(VfsFile&& file, u64 offset, u64 size);
+
+    bool IsReady() override;
+    bool IsGood() override;
+    void ResetState() override;
+    std::string GetName() override;
+    u64 GetSize() override;
+    bool Resize(u64 new_size) override;
+    std::shared_ptr<VfsDirectory> GetContainingDirectory() override;
+    bool IsWritable() override;
+    bool IsReadable() override;
+    std::vector<u8> ReadBytes(u64 offset, u64 length) override;
+    u64 WriteBytes(const std::vector<u8>& data, u64 offset) override;
+    bool Rename(const std::string& name) override;
+
+private:
+    std::unique_ptr<VfsFile> file;
+    u64 offset;
+    u64 size;
+};
+
+} // namespace FileSys
