bf0cd290a93fe3f0888bdb80ac048f5bc241d617
Zach Hilman <zachhilman@gmail.com>
2018-06-30 18:21:43 -0400

    Compile fixes

--- src/common/file_util.cpp
+++ src/common/file_util.cpp
@@ -2,6 +2,7 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <sstream>
 #include "common/assert.h"
 #include "common/common_funcs.h"
 #include "common/common_paths.h"
