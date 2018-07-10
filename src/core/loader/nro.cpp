9cb1f759ca85ee38f0e7e82a7f86aae3d05baeaf
Jarek Syrylak <hedges@resync.pl>
2018-07-10 10:25:34 +0100

    Avoid crash in IsValidVirtualAddress()

--- src/core/loader/nro.cpp
+++ src/core/loader/nro.cpp
@@ -117,8 +117,8 @@ bool AppLoader_NRO::LoadNro(const std::string& path, VAddr load_base) {
     Core::CurrentProcess()->LoadModule(codeset, load_base);
 
     std::string filename;
-    Common::SplitPath(path, nullptr, &filename, nullptr);
-    GDBStub::RegisterModule((filename+".elf").c_str(), load_base, load_base);
+    Common::SplitPath(codeset->name, nullptr, &filename, nullptr);
+    GDBStub::RegisterModule((filename + ".elf").c_str(), load_base, load_base);
 
     return true;
 }
