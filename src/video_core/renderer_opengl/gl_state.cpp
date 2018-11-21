7aa12b2467c495b55404a0c5221126a930ba6d22
Rodolfo Bogado <rodolfoosvaldobogado@gmail.com>
2018-11-21 00:40:32 -0300

    Add support for clear_flags register

--- src/video_core/renderer_opengl/gl_state.cpp
+++ src/video_core/renderer_opengl/gl_state.cpp
@@ -234,6 +234,22 @@ void OpenGLState::ApplyStencilTest() const {
     }
 }
 
+void OpenGLState::TranslateViewportToScissor() {
+    auto& current = this->viewports[0];
+    if (current.scissor.enabled) {
+        current.scissor.x = std::max(current.x, current.scissor.x);
+        current.scissor.y = std::max(current.y, current.scissor.y);
+        current.scissor.width = std::min(current.width, current.scissor.width);
+        current.scissor.height = std::min(current.height, current.scissor.height);
+    } else {
+        current.scissor.enabled = true;
+        current.scissor.x = current.x;
+        current.scissor.y = current.y;
+        current.scissor.width = current.width;
+        current.scissor.height = current.height;
+    }
+}
+
 void OpenGLState::ApplyViewport() const {
     if (GLAD_GL_ARB_viewport_array && geometry_shaders.enabled) {
         for (GLuint i = 0; i < static_cast<GLuint>(Tegra::Engines::Maxwell3D::Regs::NumViewports);
@@ -242,7 +258,9 @@ void OpenGLState::ApplyViewport() const {
             const auto& updated = viewports[i];
             if (updated.x != current.x || updated.y != current.y ||
                 updated.width != current.width || updated.height != current.height) {
-                glViewportIndexedf(i, updated.x, updated.y, updated.width, updated.height);
+                glViewportIndexedf(
+                    i, static_cast<GLfloat>(updated.x), static_cast<GLfloat>(updated.y),
+                    static_cast<GLfloat>(updated.width), static_cast<GLfloat>(updated.height));
             }
             if (updated.depth_range_near != current.depth_range_near ||
                 updated.depth_range_far != current.depth_range_far) {
@@ -270,8 +288,7 @@ void OpenGLState::ApplyViewport() const {
         const auto& updated = viewports[0];
         if (updated.x != current.x || updated.y != current.y || updated.width != current.width ||
             updated.height != current.height) {
-            glViewport(static_cast<GLint>(updated.x), static_cast<GLint>(updated.y),
-                       static_cast<GLsizei>(updated.width), static_cast<GLsizei>(updated.height));
+            glViewport(updated.x, updated.y, updated.width, updated.height);
         }
         if (updated.depth_range_near != current.depth_range_near ||
             updated.depth_range_far != current.depth_range_far) {
