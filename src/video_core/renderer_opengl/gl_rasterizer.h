33ba10591ef864e5a562334a85e8b0f403f59f62
Marcos Vitali <marcosvitali@gmail.com>
2018-11-23 03:01:33 -0300

    GPU States: Implement Polygon Offset. This is used in SMO all the time.

--- src/video_core/renderer_opengl/gl_rasterizer.h
+++ src/video_core/renderer_opengl/gl_rasterizer.h
@@ -182,6 +182,9 @@ private:
     /// Syncs Color Mask
     void SyncColorMask();
 
+    /// Syncs the polygon offsets
+    void SyncPolygonOffset();
+
     /// Check asserts for alpha testing.
     void CheckAlphaTests();
 
