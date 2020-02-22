952a9a042961c078cf8063b73a20209fa76b4957
Nguyen Dac Nam <nam.kazt.91@gmail.com>
2020-02-22 09:56:20 +0700

    clang-format

--- src/video_core/shader/decode/arithmetic.cpp
+++ src/video_core/shader/decode/arithmetic.cpp
@@ -52,7 +52,7 @@ u32 ShaderIR::DecodeArithmetic(NodeBlock& bb, u32 pc) {
         }
 
         op_b = GetOperandAbsNegFloat(op_b, false, instr.fmul.negate_b);
-        
+
         constexpr std::array FmulPostFactor = {
             1.000f, // None
             0.500f, // Devide 2
