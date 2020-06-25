56321a8cbba63e56f47843748a3a3cea2d49d3a3
lat9nq <lat9nq@virginia.edu>
2020-06-25 19:32:23 -0400

    clang format

--- src/yuzu/configuration/config.cpp
+++ src/yuzu/configuration/config.cpp
@@ -698,7 +698,8 @@ void Config::ReadSystemValues() {
 
     bool rng_seed_enabled;
     ReadSettingGlobal(rng_seed_enabled, QStringLiteral("rng_seed_enabled"), false);
-    bool rng_seed_global = global || qt_config->value(QStringLiteral("rng_seed/use_global"), true).toBool();
+    bool rng_seed_global =
+        global || qt_config->value(QStringLiteral("rng_seed/use_global"), true).toBool();
     Settings::values.rng_seed.SetGlobal(rng_seed_global);
     if (global || !rng_seed_global) {
         if (rng_seed_enabled) {
@@ -710,7 +711,8 @@ void Config::ReadSystemValues() {
 
     bool custom_rtc_enabled;
     ReadSettingGlobal(custom_rtc_enabled, QStringLiteral("custom_rtc_enabled"), false);
-    bool custom_rtc_global = global || qt_config->value(QStringLiteral("custom_rtc/use_global"), true).toBool();
+    bool custom_rtc_global =
+        global || qt_config->value(QStringLiteral("custom_rtc/use_global"), true).toBool();
     Settings::values.custom_rtc.SetGlobal(custom_rtc_global);
     if (global || !custom_rtc_global) {
         if (custom_rtc_enabled) {
