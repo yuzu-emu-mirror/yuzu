83c50acf7c53f98dbf59d231499ed138f37d2bbf
lat9nq <lat9nq@virginia.edu>
2020-07-02 13:00:16 -0400

    settings: initalize resolution factor with constructor instead of casting

--- src/core/settings.h
+++ src/core/settings.h
@@ -444,7 +444,7 @@ struct Values {
     Setting<bool> renderer_debug;
     Setting<int> vulkan_device;
 
-    Setting<u16> resolution_factor = Setting(static_cast<u16>(1));
+    Setting<u16> resolution_factor = Setting(u16(1));
     Setting<int> aspect_ratio;
     Setting<int> max_anisotropy;
     Setting<bool> use_frame_limit;
