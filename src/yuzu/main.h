f517d37a4d5621d00d3b3a26b5fcbbccbcfa8a63
VolcaEM <63682805+VolcaEM@users.noreply.github.com>
2020-07-11 16:15:50 +0200

    Add missing function declaration

--- src/yuzu/main.h
+++ src/yuzu/main.h
@@ -188,6 +188,7 @@ private slots:
     void OnGameListLoadFile(QString game_path);
     void OnGameListOpenFolder(GameListOpenTarget target, const std::string& game_path);
     void OnTransferableShaderCacheOpenFile(u64 program_id);
+    void OnConfigurationOpenFile(u64 program_id);
     void OnGameListDumpRomFS(u64 program_id, const std::string& game_path);
     void OnGameListCopyTID(u64 program_id);
     void OnGameListNavigateToGamedbEntry(u64 program_id,
