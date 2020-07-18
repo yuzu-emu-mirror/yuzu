Raibatsu/raikopon

Try to fix executable path generated in CI

.travis/linux/upload.sh
@@ -8,9 +8,9 @@ COMPRESSION_FLAGS="-cJvf"
 
 mkdir "$REV_NAME"
 
-cp build/bin/citra "$REV_NAME"
-cp build/bin/citra-room "$REV_NAME"
-cp build/bin/citra-qt "$REV_NAME"
+cp build/bin/Release/citra "$REV_NAME"
+cp build/bin/Release/citra-room "$REV_NAME"
+cp build/bin/Release/citra-qt "$REV_NAME"
 
 # We need icons on Linux for .desktop entries
 mkdir "$REV_NAME/dist"

.travis/macos/upload.sh
@@ -8,9 +8,9 @@ COMPRESSION_FLAGS="-czvf"
 
 mkdir "$REV_NAME"
 
-cp build/bin/citra "$REV_NAME"
-cp -r build/bin/citra-qt.app "$REV_NAME"
-cp build/bin/citra-room "$REV_NAME"
+cp build/bin/Release/citra "$REV_NAME"
+cp -r build/bin/Release/citra-qt.app "$REV_NAME"
+cp build/bin/Release/citra-room "$REV_NAME"
 
 # move libs into folder for deployment
 macpack "${REV_NAME}/citra-qt.app/Contents/MacOS/citra-qt" -d "../Frameworks"

yuzu-emu-mirror/yuzu

.travis/linux/upload.sh
@@ -8,7 +8,7 @@ COMPRESSION_FLAGS="-cJvf"
 
 mkdir "$REV_NAME"
 
-cp build/bin/yuzu-cmd "$REV_NAME"
-cp build/bin/yuzu "$REV_NAME"
+cp build/bin/Release/yuzu-cmd "$REV_NAME"
+cp build/bin/Release/yuzu "$REV_NAME"
 
 . .travis/common/post-upload.sh

.travis/macos/upload.sh
@@ -8,8 +8,8 @@ COMPRESSION_FLAGS="-czvf"
 
 mkdir "$REV_NAME"
 
-cp build/bin/yuzu-cmd "$REV_NAME"
-cp -r build/bin/yuzu.app "$REV_NAME"
+cp build/bin/Release/yuzu-cmd "$REV_NAME"
+cp -r build/bin/Release/yuzu.app "$REV_NAME"
 
 # move libs into folder for deployment
 macpack "${REV_NAME}/yuzu.app/Contents/MacOS/yuzu" -d "../Frameworks"