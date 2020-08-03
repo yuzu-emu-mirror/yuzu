296956caccb5bd738ceadcd35cc1bb7340c7dcc3
David Marcec <dmarcecguzman@gmail.com>
2020-08-03 17:56:24 +1000

    More agressive concept for stl containers

--- src/common/concepts.h
+++ src/common/concepts.h
@@ -4,15 +4,17 @@
 
 #pragma once
 
-#include <concepts>
-
 // Check if type is like an STL container
 template <typename T>
 concept IsSTLContainer = requires(T t) {
     typename T::value_type;
+    typename T::iterator;
+    typename T::const_iterator;
     // TODO(ogniK): Replace below is std::same_as<void> when MSVC supports it.
     t.begin();
     t.end();
+    t.cbegin();
+    t.cend();
     t.data();
     t.size();
 };
