4bc31ea849cb8a0dd9be916c570d5ce815a1f88d
comex <comexk@gmail.com>
2020-08-31 10:56:19 -0400

    vfs_real: When moving files or directories, don't assume file opening will succeed
    Found this via a warning, but it's a substantive fix.
    
    Since this is only for a cache, it should be safe to silently drop the
    file if opening fails.  I think.

--- src/core/file_sys/vfs_real.cpp
+++ src/core/file_sys/vfs_real.cpp
@@ -133,8 +133,9 @@ VirtualFile RealVfsFilesystem::MoveFile(std::string_view old_path_, std::string_
         }
 
         cache.erase(old_path);
-        file->Open(new_path, "r+b");
-        cache.insert_or_assign(new_path, std::move(file));
+        if (file->Open(new_path, "r+b")) {
+            cache.insert_or_assign(new_path, std::move(file));
+        }
     } else {
         UNREACHABLE();
         return nullptr;
@@ -214,9 +215,10 @@ VirtualDir RealVfsFilesystem::MoveDirectory(std::string_view old_path_,
         }
 
         auto file = cached.lock();
-        file->Open(file_new_path, "r+b");
         cache.erase(file_old_path);
-        cache.insert_or_assign(std::move(file_new_path), std::move(file));
+        if (file->Open(file_new_path, "r+b")) {
+            cache.insert_or_assign(std::move(file_new_path), std::move(file));
+        }
     }
 
     return OpenDirectory(new_path, Mode::ReadWrite);
