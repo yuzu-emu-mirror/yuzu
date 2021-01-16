7be3e8992152a7fdf3923fd474067fc65c4e579b
typecasto <37158342+typecasto@users.noreply.github.com>
2021-01-16 11:40:39 -0600

    Delete .ci directory

--- .ci/scripts/format/docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ex
-
-# Run clang-format
-cd /yuzu
-chmod a+x ./.ci/scripts/format/script.sh
-./.ci/scripts/format/script.sh
--- .ci/scripts/merge/yuzubot-git-config.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.email "yuzu@yuzu-emu.org"
-git config --global user.name "yuzubot"
