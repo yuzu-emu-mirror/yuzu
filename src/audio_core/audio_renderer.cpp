8b0b7168a0ad7eafd6a347fa347c495fb2d5c97e
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-02-06 09:49:43 -0500

    clang

--- src/audio_core/audio_renderer.cpp
+++ src/audio_core/audio_renderer.cpp
@@ -322,7 +322,8 @@ void AudioRenderer::ReleaseAndQueueBuffers() {
     queue_mixed_multithread.resize(released_buffers.size());
     for (const auto& tag : released_buffers) {
         queue_mixed_multithread[thread_counter] = std::async(
-            std::launch::async, [this, tag, voice_context = voice_context, splitter_context = splitter_context,
+            std::launch::async, [this, tag, voice_context = voice_context,
+                                 splitter_context = splitter_context,
                                  mix_context = mix_context] { QueueMixedBuffer(tag); });
         thread_counter++;
     }
