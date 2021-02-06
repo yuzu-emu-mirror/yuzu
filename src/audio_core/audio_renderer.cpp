66578f1ffcdce71fba0758ed0ce74b8969c7310a
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-02-06 09:54:57 -0500

    more clang
    i'm going to love condensing these commits

--- src/audio_core/audio_renderer.cpp
+++ src/audio_core/audio_renderer.cpp
@@ -321,10 +321,10 @@ void AudioRenderer::ReleaseAndQueueBuffers() {
     const auto released_buffers{audio_out->GetTagsAndReleaseBuffers(stream)};
     queue_mixed_multithread.resize(released_buffers.size());
     for (const auto& tag : released_buffers) {
-        queue_mixed_multithread[thread_counter] = std::async(
-            std::launch::async, [this, tag, voice_context = voice_context,
-                                 splitter_context = splitter_context,
-                                 mix_context = mix_context] { QueueMixedBuffer(tag); });
+        queue_mixed_multithread[thread_counter] = 
+            std::async(std::launch::async, [this, tag, voice_context = voice_context,
+                                            splitter_context = splitter_context,
+                                            mix_context = mix_context] { QueueMixedBuffer(tag); });
         thread_counter++;
     }
     for (std::size_t thread = 0; thread < released_buffers.size(); thread++) {
