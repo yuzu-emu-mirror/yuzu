dc9517051d1fe7019e2f35f613d8fce3425f9498
Hanjie Wu <nagi@zju.edu.cn>
2021-10-04 00:46:59 +0800

    fix AESCipher encryption & use unique_ptr for EVP_CIPHER_CONTEXT

--- src/core/crypto/aes_util_openssl.cpp
+++ src/core/crypto/aes_util_openssl.cpp
@@ -1,6 +1,7 @@
 #ifdef YUZU_USE_OPENSSL_CRYPTO
 
 #include <map>
+#include <memory>
 #include <vector>
 #include <openssl/evp.h>
 #include "common/assert.h"
@@ -22,11 +23,15 @@ NintendoTweak CalculateNintendoTweak(std::size_t sector_id) {
 }
 } // Anonymous namespace
 
+using EvpCipherContext = std::unique_ptr<EVP_CIPHER_CTX, decltype(&EVP_CIPHER_CTX_free)>;
+
 struct CipherContext {
     const EVP_CIPHER* cipher;
-    EVP_CIPHER_CTX* ctx;
+    EvpCipherContext ctx;
     std::vector<u8> key;
     std::vector<u8> iv;
+
+    CipherContext(EVP_CIPHER_CTX* ctx_in) : ctx(ctx_in, EVP_CIPHER_CTX_free) {}
 };
 
 const static std::map<Mode, decltype(&EVP_aes_128_ctr)> cipher_map = {
@@ -34,37 +39,42 @@ const static std::map<Mode, decltype(&EVP_aes_128_ctr)> cipher_map = {
 
 template <typename Key, std::size_t KeySize>
 Crypto::AESCipher<Key, KeySize>::AESCipher(Key key, Mode mode)
-    : ctx(std::make_unique<CipherContext>()) {
-    ctx->ctx = EVP_CIPHER_CTX_new();
-    ASSERT_MSG((ctx->ctx != NULL), "Failed to initialize OpenSSL ciphers.");
+    : ctx(std::make_unique<CipherContext>(EVP_CIPHER_CTX_new())) {
+    ASSERT_MSG((ctx->ctx.get() != NULL), "Failed to initialize OpenSSL ciphers.");
     ctx->cipher = cipher_map.at(mode)();
     ctx->key.resize(KeySize);
     std::memcpy(ctx->key.data(), key.data(), KeySize);
 }
 
 template <typename Key, std::size_t KeySize>
-AESCipher<Key, KeySize>::~AESCipher() {
-    EVP_CIPHER_CTX_free(ctx->ctx);
-}
+AESCipher<Key, KeySize>::~AESCipher() = default;
 
 template <typename Key, std::size_t KeySize>
 void AESCipher<Key, KeySize>::Transcode(const u8* src, std::size_t size, u8* dest, Op op) const {
-    EVP_CIPHER_CTX_reset(ctx->ctx);
-    EVP_CipherInit(ctx->ctx, ctx->cipher, ctx->key.data(), ctx->iv.data(),
+    EVP_CIPHER_CTX_reset(ctx->ctx.get());
+    EVP_CipherInit(ctx->ctx.get(), ctx->cipher, ctx->key.data(), ctx->iv.data(),
                    op == Op::Encrypt ? 1 : 0);
-    EVP_CIPHER_CTX_set_padding(ctx->ctx, 0);
+    EVP_CIPHER_CTX_set_padding(ctx->ctx.get(), 0);
 
     int written, last_written;
     if (EVP_CIPHER_mode(ctx->cipher) == EVP_CIPH_XTS_MODE) {
-        EVP_CipherUpdate(ctx->ctx, dest, &written, src, (int)size);
+        EVP_CipherUpdate(ctx->ctx.get(), dest, &written, src, (int)size);
         if (written != (int)size) {
             LOG_WARNING(Crypto, "Not all data was decrypted requested={:016X}, actual={:016X}.",
                         size, written);
         }
     } else {
-        EVP_CipherUpdate(ctx->ctx, dest, &written, src, (int)size);
-        EVP_CipherFinal(ctx->ctx, dest + written, &last_written);
-        if (written + last_written != (int)size) {
+        std::size_t block_size = EVP_CIPHER_block_size(ctx->cipher);
+        std::size_t remain = size % block_size;
+        EVP_CipherUpdate(ctx->ctx.get(), dest, &written, src, (int)(size - remain));
+        if (remain != 0) {
+            std::vector<u8> block(block_size);
+            std::memcpy(block.data(), src + size - remain, remain);
+            EVP_CipherUpdate(ctx->ctx.get(), dest + written, &last_written, block.data(),
+                             block_size);
+            written += last_written;
+        }
+        if (written != (int)size) {
             LOG_WARNING(Crypto, "Not all data was decrypted requested={:016X}, actual={:016X}.",
                         size, written + last_written);
         }
