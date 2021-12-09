8fb5f2322690b914097ef079fe87a0dc9a3fe30d
Hanjie Wu <nagi@zju.edu.cn>
2021-12-10 01:36:50 +0800

    avoid EVP_CIPHER_CTX_reset() & add speed test

--- src/core/crypto/aes_util_openssl.cpp
+++ src/core/crypto/aes_util_openssl.cpp
@@ -37,11 +37,13 @@ using EvpCipherContext = std::unique_ptr<EVP_CIPHER_CTX, EvpCipherContextFree>;
 
 struct CipherContext {
     const EVP_CIPHER* cipher;
-    EvpCipherContext ctx;
+    EvpCipherContext encrypt_ctx;
+    EvpCipherContext decrypt_ctx;
     std::vector<u8> key;
     std::vector<u8> iv;
 
-    CipherContext(EVP_CIPHER_CTX* ctx_in) : ctx(ctx_in) {}
+    CipherContext(EVP_CIPHER_CTX* encrypt_ctx_in, EVP_CIPHER_CTX* decrypt_ctx_in)
+        : encrypt_ctx(encrypt_ctx_in), decrypt_ctx(decrypt_ctx_in) {}
 };
 
 const static std::map<Mode, decltype(&EVP_aes_128_ctr)> cipher_map = {
@@ -49,11 +51,16 @@ const static std::map<Mode, decltype(&EVP_aes_128_ctr)> cipher_map = {
 
 template <typename Key, std::size_t KeySize>
 Crypto::AESCipher<Key, KeySize>::AESCipher(Key key, Mode mode)
-    : ctx(std::make_unique<CipherContext>(EVP_CIPHER_CTX_new())) {
-    ASSERT_MSG((ctx->ctx.get() != NULL), "Failed to initialize OpenSSL ciphers.");
+    : ctx(std::make_unique<CipherContext>(EVP_CIPHER_CTX_new(), EVP_CIPHER_CTX_new())) {
+    ASSERT_MSG((ctx->encrypt_ctx.get() != NULL), "Failed to initialize OpenSSL ciphers.");
+    ASSERT_MSG((ctx->decrypt_ctx.get() != NULL), "Failed to initialize OpenSSL ciphers.");
     ctx->cipher = cipher_map.at(mode)();
     ctx->key.resize(KeySize);
     std::memcpy(ctx->key.data(), key.data(), KeySize);
+    EVP_EncryptInit(ctx->encrypt_ctx.get(), ctx->cipher, ctx->key.data(), nullptr);
+    EVP_DecryptInit(ctx->decrypt_ctx.get(), ctx->cipher, ctx->key.data(), nullptr);
+    EVP_CIPHER_CTX_set_padding(ctx->encrypt_ctx.get(), 0);
+    EVP_CIPHER_CTX_set_padding(ctx->decrypt_ctx.get(), 0);
 }
 
 template <typename Key, std::size_t KeySize>
@@ -61,14 +68,11 @@ AESCipher<Key, KeySize>::~AESCipher() = default;
 
 template <typename Key, std::size_t KeySize>
 void AESCipher<Key, KeySize>::Transcode(const u8* src, std::size_t size, u8* dest, Op op) const {
-    EVP_CIPHER_CTX_reset(ctx->ctx.get());
-    EVP_CipherInit(ctx->ctx.get(), ctx->cipher, ctx->key.data(), ctx->iv.data(),
-                   op == Op::Encrypt ? 1 : 0);
-    EVP_CIPHER_CTX_set_padding(ctx->ctx.get(), 0);
-
+    EVP_CIPHER_CTX* cipher_ctx =
+        op == Op::Encrypt ? ctx->encrypt_ctx.get() : ctx->decrypt_ctx.get();
     int written, last_written;
     if (EVP_CIPHER_mode(ctx->cipher) == EVP_CIPH_XTS_MODE) {
-        EVP_CipherUpdate(ctx->ctx.get(), dest, &written, src, static_cast<int>(size));
+        EVP_CipherUpdate(cipher_ctx, dest, &written, src, static_cast<int>(size));
         if (written != static_cast<int>(size)) {
             LOG_WARNING(Crypto, "Not all data was decrypted requested={:016X}, actual={:016X}.",
                         size, written);
@@ -76,11 +80,11 @@ void AESCipher<Key, KeySize>::Transcode(const u8* src, std::size_t size, u8* des
     } else {
         std::size_t block_size = EVP_CIPHER_block_size(ctx->cipher);
         std::size_t remain = size % block_size;
-        EVP_CipherUpdate(ctx->ctx.get(), dest, &written, src, static_cast<int>(size - remain));
+        EVP_CipherUpdate(cipher_ctx, dest, &written, src, static_cast<int>(size - remain));
         if (remain != 0) {
             std::vector<u8> block(block_size);
             std::memcpy(block.data(), src + size - remain, remain);
-            EVP_CipherUpdate(ctx->ctx.get(), dest + written, &last_written, block.data(),
+            EVP_CipherUpdate(cipher_ctx, dest + written, &last_written, block.data(),
                              static_cast<int>(block_size));
             written += last_written;
         }
@@ -106,6 +110,8 @@ template <typename Key, std::size_t KeySize>
 void AESCipher<Key, KeySize>::SetIV(std::span<const u8> data) {
     ctx->iv.resize(data.size());
     std::memcpy(ctx->iv.data(), data.data(), data.size());
+    EVP_EncryptInit(ctx->encrypt_ctx.get(), nullptr, nullptr, ctx->iv.data());
+    EVP_DecryptInit(ctx->decrypt_ctx.get(), nullptr, nullptr, ctx->iv.data());
 }
 
 template class AESCipher<Key128>;
