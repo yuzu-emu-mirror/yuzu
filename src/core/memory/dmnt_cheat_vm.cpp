cff88cedf5d56b30eeff219c9a58bd4982920f9b
Bastian Germann <bage@debian.org>
2022-04-23 11:09:57 +0200

    Relicense the leftover GPL-2.0-only files
    The relicensing can be done because the Atmosphère-NX upstream has the
    following license addition:
    
    """
    The yuzu Nintendo Switch emulator and the Ryujinx Team and Contributors are
    exempt from GPLv2 licensing. They are permitted, each at their individual
    discretion, to instead license any source code authored for the Atmosphère
    project as either GPLv2 or later or the MIT license. In doing so, they may
    alter, supplement, or entirely remove the copyright notice for each file
    they choose to relicense. Neither the Atmosphère project nor its individual
    contributors shall assert their moral rights against any of the
    aforementioned projects.
    """
    
    Link: https://github.com/Atmosphere-NX/Atmosphere/blob/0bb2c0a04f0eca4/README.md
    Fixes: #8242

--- src/core/memory/dmnt_cheat_vm.cpp
+++ src/core/memory/dmnt_cheat_vm.cpp
@@ -1,26 +1,9 @@
-/*
- * Copyright (c) 2018-2019 Atmosphère-NX
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright (c) 2018-2019 Atmosphère-NX
+// Modifications Copyright 2019 yuzu emulator team
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
 
-/*
- * Adapted by DarkLordZach for use/interaction with yuzu
- *
- * Modifications Copyright 2019 yuzu emulator team
- * Licensed under GPLv2 or any later version
- * Refer to the license.txt file included.
- */
+// Adapted by DarkLordZach for use/interaction with yuzu
 
 #include "common/assert.h"
 #include "common/scope_exit.h"
