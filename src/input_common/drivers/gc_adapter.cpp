2c6104f11bab69270eaca22b82bc1a1d3f2e0145
Levi Behunin <l3ehunin@gmail.com>
2022-05-02 00:06:19 -0600

    Update gc_adapter.cpp
    GCC11.2 complains cannot find 'libusb.h' during a fresh build

--- src/input_common/drivers/gc_adapter.cpp
+++ src/input_common/drivers/gc_adapter.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <fmt/format.h>
-#include <libusb.h>
+#include <libusb-1.0/libusb.h>
 
 #include "common/logging/log.h"
 #include "common/param_package.h"
