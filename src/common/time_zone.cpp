20f14b50fd9bd6a6b2d954b9e33c8598f37513d1
lat9nq <22451773+lat9nq@users.noreply.github.com>
2023-05-23 18:32:28 -0400

    common: Move system time zone string detection
    Moves it from Settings to Common::TimeZone, since this algorithm doesn't
    depend on the setting. It also lets us use it in other libraries.

--- src/common/time_zone.cpp
+++ src/common/time_zone.cpp
@@ -4,6 +4,8 @@
 #include <chrono>
 #include <iomanip>
 #include <sstream>
+#include <fmt/chrono.h>
+#include <fmt/core.h>
 
 #include "common/logging/log.h"
 #include "common/time_zone.h"
@@ -18,10 +20,7 @@ static std::string GetOsTimeZoneOffset() {
     const std::time_t t{std::time(nullptr)};
     const std::tm tm{*std::localtime(&t)};
 
-    std::stringstream ss;
-    ss << std::put_time(&tm, "%z"); // Get the current timezone offset, e.g. "-400", as a string
-
-    return ss.str();
+    return fmt::format("{:%z}", tm);
 }
 
 static int ConvertOsTimeZoneOffsetToInt(const std::string& timezone) {
@@ -43,6 +42,43 @@ std::chrono::seconds GetCurrentOffsetSeconds() {
     seconds += (offset % 100) * 60;        // Convert minute component to seconds
 
     return std::chrono::seconds{seconds};
+}
+
+std::string FindSystemTimeZone() {
+#if defined(MINGW)
+    // MinGW has broken strftime -- https://sourceforge.net/p/mingw-w64/bugs/793/
+    // e.g. fmt::format("{:%z}") -- returns "Eastern Daylight Time" when it should be "-0400"
+    return timezones[0];
+#else
+    static std::string system_time_zone_cached{};
+    if (!system_time_zone_cached.empty()) {
+        return system_time_zone_cached;
+    }
+
+    const auto now = std::time(nullptr);
+    const struct std::tm& local = *std::localtime(&now);
+
+    const auto system_offset = GetCurrentOffsetSeconds().count() - (local.tm_isdst ? 3600 : 0);
+
+    int min = std::numeric_limits<int>::max();
+    int min_index = -1;
+    for (u32 i = 2; i < offsets.size(); i++) {
+        // Skip if system is celebrating DST but considered time zone does not
+        if (local.tm_isdst && !dst[i]) {
+            continue;
+        }
+
+        const auto offset = offsets[i];
+        const int difference = std::abs(std::abs(offset) - std::abs(system_offset));
+        if (difference < min) {
+            min = difference;
+            min_index = i;
+        }
+    }
+
+    system_time_zone_cached = timezones[min_index];
+    return timezones[min_index];
+#endif
 }
 
 } // namespace Common::TimeZone
