42f7f6b6fdd9f40e9709b790f0a6df0007307333
Fernando Sahmkow <fsahmkow27@gmail.com>
2024-01-05 01:51:42 +0100

    GPU: Implement channel scheduling.

--- src/video_core/gpu.cpp
+++ src/video_core/gpu.cpp
@@ -401,6 +401,14 @@ std::shared_ptr<Control::ChannelState> GPU::AllocateChannel() {
     return impl->AllocateChannel();
 }
 
+Tegra::Control::Scheduler& GPU::Scheduler() {
+    return *impl->scheduler;
+}
+
+const Tegra::Control::Scheduler& GPU::Scheduler() const {
+    return *impl->scheduler;
+}
+
 void GPU::InitChannel(Control::ChannelState& to_init) {
     impl->InitChannel(to_init);
 }
